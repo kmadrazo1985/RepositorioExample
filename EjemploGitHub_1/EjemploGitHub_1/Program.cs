24c550131cb21f42ebe085b7c3337cdd37a10364
kmadrazo1985 <66638674+kmadrazo1985@users.noreply.github.com>
2020-06-08 22:39:07 -0500

    primera entrega codigo fuente

---
+++ EjemploGitHub_1/EjemploGitHub_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjemploGitHub_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
